2faef3de15e747620f087f14266483f4d7643c5a
Lucas Linden <lucas.linden@cwi.com.br>
2022-11-29 09:58:43 -0300

    device farm

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,25 +3,38 @@ const allure = require("allure-commandline");
 const video = require("wdio-video-reporter");
 
 exports.config = {
-  hostname: "localhost",
-  port: 4723,
-  path: "/wd/hub",
+  //hostname: "localhost",
+  //port: 4723,
+  //path: "/wd/hub",
+  user: "ll_UQluah",
+  key: "mvDcLTLsveY6FP19yP42",
+  //services: ['appium'],
+  services: ["browserstack"],
   specs: ["./test/specs/**/*.spec.js"],
   framework: "mocha",
   capabilities: [
     {
       platformName: "Android",
-      platformVersion: "9.0",
-      deviceName: "Pixel 2 API 28",
-      automationName: "UiAutomator2",
-      app: join(process.cwd(), "./app/android/wcandroid-11.0.apk"),
-      appWaitActivity: "com.woocommerce.android.ui.login.LoginActivity",
-      adbExecTimeout: 40000,
+      //platformVersion: "9.0",
+      //deviceName: "Pixel 2 API 28",
+      //automationName: "UiAutomator2",
+      //app: join(process.cwd(), "./app/android/wcandroid-11.0.apk"),
+      //appWaitActivity: "com.woocommerce.android.ui.login.LoginActivity",
+      //adbExecTimeout: 40000,
+      project: "Meu primeiro projeto em Device Farm",
+      build: "browserstack-build-1",
+      name: "teste_ebac_shop",
+      device: "Samsung Galaxy S22 Ultra",
+      os_version: "12.0",
+      app:
+        process.env.BROWSERSTACK_APP_ID ||
+        "bs://a142723ba695d686187ccb213634e286c4f77b24",
+      "browserstack.local": false,
     },
   ],
-  waitforTimeout: 30000,
+  waitforTimeout: 50000,
   mochaOpts: {
-    timeout: 30000,
+    timeout: 50000,
   },
   reporters: [
     [
